2adac4cb30f1ceadf29567d2e2f20c985272f776
Jay <34258275+sell@users.noreply.github.com>
2020-12-08 23:16:08 -0500

    changed username -> email

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { sig } = require("./utils/sig");
     sig();
 
     await dc.initialize();
-    // here is where you enter your password and email
-    await dc.login('username', 'password')
+    // here is where you enter your email and password
+    await dc.login('email', 'password')
 
     await dc.likeChannelProcess('server id', 'channel id', 1) // 1 = 1 minute
 
